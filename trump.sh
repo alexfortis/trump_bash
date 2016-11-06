d8a18d549774446f4b8c49a6b9698cf955d86109
alexfortis <alexfortis@users.noreply.github.com>
2016-11-06 10:15:46 -0500

    Initial commit

---
+++ trump.sh
@@ -0,0 +1,20 @@
+trap "killall -SIGINT trump" SIGINT
+trap "killall -SIGQUIT trump" SIGQUIT
+FILE="trump"
+printf "%s\n\n" "Donald Trump is stuck in a Terminal and can\'t get out! Of course, we don\'t want him out, but we can still communicate with him. He will send out tweets every few seconds, and we can send him signals, which he will respond to."
+if [ -f $FILE ]; then
+    rm $FILE
+fi
+make $FILE
+
+./$FILE &
+while true
+do
+    if pgrep "$FILE" > /dev/null;
+    then	
+	read signal
+	killall -$signal $FILE
+    else
+	break
+    fi
+done
---
+++ trump_sim.c
@@ -0,0 +1,130 @@
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+
+//when the controlling terminal quits
+void hupHandler(int sig) {
+  printf("It is so nice that the shackles have been taken off me and I can now fight for America the way I want to.\n");
+}
+
+//when the interrupt signal (ctrl+C) is sent
+void intHandler(int sig) {
+  printf("She went over time, I can go over too.\n");
+}
+
+//when the quit signal (ctrl+\) is sent
+void quitHandler(int sig) {
+  printf("I\'d never withdraw. I\'ve never withdrawn in my life. No, I\'m not quitting. I have tremendous support.\n");
+}
+
+//bus error (misaligned pointer)
+void busHandler(int sig) {
+  printf("You\'re fired.\n");
+}
+
+//floating point exception
+void fpeHandler(int sig) {
+  printf("I\'m very rich, believe me.\n");
+}
+
+//segmentation fault
+void segvHandler(int sig) {
+  printf("In many cases, I probably identify more as Democrat. It just seems that the economy does better under the Democrats than the Republicans.\n");
+}
+
+//when a child (process) dies
+void chldHandler(int sig) {
+  int status = 0;
+  pid_t childPid = wait(&status);
+  printf("%d is a war hero because he was captured. I prefer processes that aren\'t captured, okay?\n", childPid);
+}
+
+//When the OS pauses this process
+void stopHandler(int sig) {
+  printf("You\'re the puppet.\n");
+}
+
+//when the OS gets this process back
+void contHandler(int sig) {
+  printf("It\'s just locker room talk.\n");
+}
+
+void ioHandler(int sig) {
+  printf("WRONG.\n");
+}
+
+char *fix(char *broken) {
+  char *ret = (char *)calloc(strlen(broken), sizeof(char));
+  int counter = 0;
+  for(unsigned i = 0; i < strlen(broken); i++) {
+    if(broken[i] == '\\') {
+      if(broken[i+1] == 'n') {
+	ret[counter] = '\n';
+	i++;
+      }
+    }
+    else if(broken[i] == '&') {
+      if((broken[i+1] == 'a') &&
+	 (broken[i+2] == 'm') &&
+	 (broken[i+3] == 'p') &&
+	 (broken[i+4] == ';')) {
+	i += 4;
+	ret[counter] = '&';
+      }
+    }
+    else {
+      ret[counter] = broken[i];
+    }
+    counter++;
+  }
+  return ret;
+}
+
+int main() {
+  signal(SIGHUP, hupHandler);
+  signal(SIGINT, intHandler);
+  signal(SIGQUIT, quitHandler);
+  signal(SIGBUS, busHandler);
+  signal(SIGFPE, fpeHandler);
+  signal(SIGSEGV, segvHandler);
+  signal(SIGCHLD, chldHandler);
+  signal(SIGSTOP, stopHandler);
+  signal(SIGCONT, contHandler);
+  signal(SIGIO, ioHandler);
+  //fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_SETFL) | O_ASYNC);
+  size_t n = 141;
+  FILE *tweetFile = fopen("trump_tweets.txt", "r");
+  if(tweetFile) {
+    char *buf = (char *)calloc(n, sizeof(char));
+    getline(&buf, &n, tweetFile);
+    int numLines = atoi(buf);
+    char *tweets[numLines];
+    tweets[0] = buf;
+    for(int i = 0; i < numLines; i++) {
+      //free(buf);
+      buf = (char *)calloc(n, sizeof(char));
+      getline(&buf, &n, tweetFile);
+      tweets[i] = buf;
+    }
+    //free(buf);
+    fclose(tweetFile);
+    while(1) {
+      srand(time(NULL));
+      int tweetIndex = rand()%numLines;
+      char *tweet = fix(tweets[tweetIndex]);
+      sleep(3);
+      printf("%s\n", tweet);
+    }
+  }
+  else {
+    printf("error: tweet file is NULL\n");
+  }
+}
