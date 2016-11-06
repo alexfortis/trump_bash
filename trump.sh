3d73bed5a53433a63f38ae6f94794c8966bf63b0
alexfortis <alexfortis@users.noreply.github.com>
2016-11-06 12:54:38 -0500

    Added GUI, changed SIGCHLD
    Added GUI using zenity and changed output of SIGCHLD handler.

--- trump.sh
+++ trump.sh
@@ -1,6 +1,7 @@
 trap "killall -SIGINT trump" SIGINT
 trap "killall -SIGQUIT trump" SIGQUIT
 FILE="trump"
+TITLE="^[Tt]r(ump_[Bb])?ash$"
 printf "%s\n\n" "Donald Trump is stuck in a Terminal and can\'t get out! Of course, we don\'t want him out, but we can still communicate with him. He will send out tweets every few seconds, and we can send him signals, which he will respond to."
 if [ -f $FILE ]; then
     rm $FILE
@@ -8,11 +9,19 @@ fi
 make $FILE
 
 ./$FILE &
-while true
+sigs=(SIGHUP SIGINT SIGQUIT SIGBUS SIGFPE SIGSEGV SIGCHLD SIGSTOP SIGCONT SIGIO)
+while true;
 do
     if pgrep "$FILE" > /dev/null;
-    then	
-	read signal
+    then
+	break
+    fi
+done
+while true;
+do
+    if pgrep "$FILE" > /dev/null;
+    then
+	signal=$(zenity --entry --title $TITLE --text "${sigs[@]}" --text "Choose a signal to send to Mr. Trump, or click Cancel to quit.")
 	killall -$signal $FILE
     else
 	break
