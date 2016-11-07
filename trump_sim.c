5107f2de4e55af648d3619727f349e4b64ad43e9
Alexander Strong <alex.j.strong@gmail.com>
2016-11-07 03:08:34 -0500

    cleaned up signal handler code

--- trump_sim.c
+++ trump_sim.c
@@ -11,67 +11,50 @@
 #include <time.h>
 #include <fcntl.h>
 
-//when the controlling terminal quits
-void hupHandler(int sig) {
-  printf("It is so nice that the shackles have been taken off me and I can now fight for America the way I want to.\n\n");
-  sleep(3);
-}
-
-//when the interrupt signal (ctrl+C) is sent
-void intHandler(int sig) {
-  printf("She went over time, I can go over too.\n\n");
-  sleep(3);
-}
-
-//when the quit signal (ctrl+\) is sent
-void quitHandler(int sig) {
-  printf("I\'d never withdraw. I\'ve never withdrawn in my life. No, I\'m not quitting. I have tremendous support.\n\n");
-  sleep(3);
-}
-
-//bus error (misaligned pointer)
-void busHandler(int sig) {
-  printf("You\'re fired.\n\n");
-  sleep(3);
-}
-
-//floating point exception
-void fpeHandler(int sig) {
-  printf("I\'m very rich, believe me.\n\n");
-  sleep(3);
-}
-
-//segmentation fault
-void segvHandler(int sig) {
-  printf("In many cases, I probably identify more as Democrat. It just seems that the economy does better under the Democrats than the Republicans.\n\n");
-  sleep(3);
-}
-
-//when a child (process) dies
-void chldHandler(int sig) {
+//use this to handle all incoming signals
+void handler(int sig) {
   int status = 0;
-  pid_t childPid = wait(&status);
-  printf("I won\'t do anything to take care of them. I\'ll supply funds and she\'ll take care of the kids.\n\n");
+  pid_t childPid;
+  switch(sig) {
+  case SIGHUP: //hangup
+    printf("It is so nice that the shackles have been taken off me and I can now fight for America the way I want to.\n\n");
+    break;
+  case SIGINT: //interrupt
+    printf("She went over time, I can go over too.\n\n");
+    break;
+  case SIGQUIT: //quit
+    printf("I\'d never withdraw. I\'ve never withdrawn in my life. No, I\'m not quitting. I have tremendous support.\n\n");
+    break;
+  case SIGBUS: //bus error
+    printf("You\'re fired.\n\n");
+    break;
+  case SIGFPE: //floating point exception
+    printf("I\'m very rich, believe me.\n\n");
+    break;
+  case SIGSEGV: //segmentation violation
+    printf("In many cases, I probably identify more as Democrat. It just seems that the economy does better under the Democrats than the Republicans.\n\n");
+    break;
+  case SIGCHLD: //child process dies
+    childPid = wait(&status);
+    printf("I won\'t do anything to take care of them. I\'ll supply funds and she\'ll take care of the kids.\n\n");
+    break;
+  case SIGTERM: //terminate
+    printf("You\'re the puppet.\n\n");
+    break;
+  case SIGCONT: //continue
+    printf("It\'s just locker room talk.\n\n");
+    break;
+  case SIGIO: //I/O ready
+    printf("WRONG.\n\n");
+    break;
+  default: //just in case
+    printf("You\'re wrong.\n\n");
+  }
+  //give the user time to read Trump's response
   sleep(3);
 }
 
-//When the OS pauses this process
-void termHandler(int sig) {
-  printf("You\'re the puppet.\n\n");
-  sleep(3);
-}
-
-//when the OS gets this process back
-void contHandler(int sig) {
-  printf("It\'s just locker room talk.\n\n");
-  sleep(3);
-}
-
-void ioHandler(int sig) {
-  printf("WRONG.\n\n");
-  sleep(3);
-}
-
+//take care of extraneous escape characters and such
 char *fix(char *broken) {
   char *ret = (char *)calloc(strlen(broken), sizeof(char));
   int counter = 0;
@@ -100,20 +83,22 @@ char *fix(char *broken) {
 }
 
 int main() {
-  signal(SIGHUP, hupHandler);
-  signal(SIGINT, intHandler);
-  signal(SIGQUIT, quitHandler);
-  signal(SIGBUS, busHandler);
-  signal(SIGFPE, fpeHandler);
-  signal(SIGSEGV, segvHandler);
-  signal(SIGCHLD, chldHandler);
-  signal(SIGTERM, termHandler);
-  signal(SIGCONT, contHandler);
-  signal(SIGIO, ioHandler);
-  //fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_SETFL) | O_ASYNC);
+  //set up the signal handlers
+  signal(SIGHUP, handler);
+  signal(SIGINT, handler);
+  signal(SIGQUIT, handler);
+  signal(SIGBUS, handler);
+  signal(SIGFPE, handler);
+  signal(SIGSEGV, handler);
+  signal(SIGCHLD, handler);
+  signal(SIGTERM, handler);
+  signal(SIGCONT, handler);
+  signal(SIGIO, handler);
+  //get the file
   size_t n = 141;
   FILE *tweetFile = fopen("trump_tweets.txt", "r");
   if(tweetFile) {
+    //read in all the lines from the file
     char *buf = (char *)calloc(n, sizeof(char));
     getline(&buf, &n, tweetFile);
     int numLines = atoi(buf);
@@ -125,6 +110,7 @@ int main() {
       tweets[i] = buf;
     }
     fclose(tweetFile);
+    //every 3 seconds, print a random tweet from tweetFile to stdout
     while(1) {
       srand(time(NULL));
       int tweetIndex = rand()%numLines;
@@ -134,6 +120,7 @@ int main() {
       free(tweet);
     }
   }
+  //error checking
   else {
     printf("error: tweet file is NULL\n");
   }
