f5677e1a3066796e85e05a98bc2c8ef89527f61d
Alexander Strong <alex.j.strong@gmail.com>
2016-11-07 02:38:49 -0500

    added sleep after printing in signal handlers

--- trump_sim.c
+++ trump_sim.c
@@ -14,31 +14,37 @@
 //when the controlling terminal quits
 void hupHandler(int sig) {
   printf("It is so nice that the shackles have been taken off me and I can now fight for America the way I want to.\n\n");
+  sleep(3);
 }
 
 //when the interrupt signal (ctrl+C) is sent
 void intHandler(int sig) {
   printf("She went over time, I can go over too.\n\n");
+  sleep(3);
 }
 
 //when the quit signal (ctrl+\) is sent
 void quitHandler(int sig) {
   printf("I\'d never withdraw. I\'ve never withdrawn in my life. No, I\'m not quitting. I have tremendous support.\n\n");
+  sleep(3);
 }
 
 //bus error (misaligned pointer)
 void busHandler(int sig) {
   printf("You\'re fired.\n\n");
+  sleep(3);
 }
 
 //floating point exception
 void fpeHandler(int sig) {
   printf("I\'m very rich, believe me.\n\n");
+  sleep(3);
 }
 
 //segmentation fault
 void segvHandler(int sig) {
   printf("In many cases, I probably identify more as Democrat. It just seems that the economy does better under the Democrats than the Republicans.\n\n");
+  sleep(3);
 }
 
 //when a child (process) dies
@@ -46,20 +52,24 @@ void chldHandler(int sig) {
   int status = 0;
   pid_t childPid = wait(&status);
   printf("I won\'t do anything to take care of them. I\'ll supply funds and she\'ll take care of the kids.\n\n");
+  sleep(3);
 }
 
 //When the OS pauses this process
 void termHandler(int sig) {
   printf("You\'re the puppet.\n\n");
+  sleep(3);
 }
 
 //when the OS gets this process back
 void contHandler(int sig) {
   printf("It\'s just locker room talk.\n\n");
+  sleep(3);
 }
 
 void ioHandler(int sig) {
   printf("WRONG.\n\n");
+  sleep(3);
 }
 
 char *fix(char *broken) {
