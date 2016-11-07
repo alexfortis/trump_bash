f912306670e6c21619f6d3ffa70b2646bfc58de8
Alexander Strong <alex.j.strong@gmail.com>
2016-11-07 00:59:50 -0500

    Fixed possible bugs in trump.sh and made compilation silent.

--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 trump: trump_sim.c
-	gcc -g -std=gnu11 -Wall -Wextra -pedantic -o trump trump_sim.c
+	@gcc -g -std=gnu11 -Wall -Wextra -pedantic -o trump trump_sim.c
 
--- trump.sh
+++ trump.sh
@@ -1,5 +1,5 @@
-trap "killall -SIGINT trump" SIGINT
-trap "killall -SIGQUIT trump" SIGQUIT
+#!/bin/bash
+
 FILE="trump"
 TITLE="^[Tt]r(ump_[Bb])?ash$"
 printf "%s\n\n" "Donald Trump is stuck in a Terminal and can\'t get out! Of course, we don\'t want him out, but we can still communicate with him. He will send out tweets every few seconds, and we can send him signals, which he will respond to."
@@ -10,21 +10,21 @@ make $FILE
 
 ./$FILE &
 sigs=(SIGQUIT SIGHUP SIGINT SIGQUIT SIGBUS SIGFPE SIGSEGV SIGCHLD SIGSTOP SIGCONT SIGIO)
+running=`ps --no-headers -C $FILE | wc -l`
 while true;
 do
-    if pgrep "$FILE" > /dev/null;
+    if [ ${running} -gt 0 ];
     then
 	break
     fi
 done
-while true;
+while [ ${running} -gt 0 ];
 do
-    if pgrep "$FILE" > /dev/null;
-    then
-	signal=$(zenity --entry --title $TITLE --text "${sigs[@]}" --text "Choose a signal to send to Mr. Trump, or click Cancel to quit.")
-	killall -$signal $FILE
-    else
+    signal=$(zenity --entry --title $TITLE --text "${sigs[@]}" --text "Choose a signal to send to Mr. Trump, or click Cancel to quit.")
+    if [[ $? -eq 1 ]]; then
+	killall -KILL $FILE
 	break
     fi
+    killall -$signal $FILE
 done
 printf "%s is a mess. %s is a big fat mistake.\n" $USER $USER
